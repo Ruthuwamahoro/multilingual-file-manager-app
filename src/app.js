d8dfe030c5349e6e20c0c05c2ed28af15756c76c
agigibairene <i.agigiba@alustudent.com>
2024-11-23 20:32:50 +0200

    Authentication

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/user";
+
 dotenv.config()
 const app = express();
-connectDB()
+
+
+connectDB();
+
+app.use("/users", userRoutes)
+
 export default app;
---
+++ src/utils/validate.js
@@ -0,0 +1,27 @@
+const  Joi  = require("joi");
+const registerSchema = Joi.object({
+    email: Joi.string().email().required(),
+    fullName: Joi.string().max(100).required(),
+    gender: Joi.string().max(50).required(),
+    telephone: Joi.string().min(4).max(15).required(),
+    password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{8,}$')).required().messages({
+        'string.pattern.base': 'Password must contain 8-30 characters'
+    }),
+    ConfirmPassword: Joi.string().valid(Joi.ref("password")).messages({
+        'any.only': 'Confirm password must be same as password'
+    })
+})
+const options = {
+    abortEarly: false,
+    allowUnknown: true,
+    stripUnknown: true
+}
+
+const ProductSchema = Joi.object({
+    name: Joi.string().max(20).required(),
+    description: Joi.string().min(4).max(15),
+    price: Joi.number().min(4).max(15).required(),
+})
+
+
+module.exports = { registerSchema, options, ProductSchema}
