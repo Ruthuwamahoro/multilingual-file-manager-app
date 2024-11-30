1b7941bc83ad7cb1e5aeeb33963c0ecf41359cb6
agigibairene <i.agigiba@alustudent.com>
2024-11-30 22:56:43 +0200

    directory test file

---
+++ tests/directory.test.js
@@ -0,0 +1,95 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app');
+const Directory = require('../src/models/directory.model');
+const { expect } = chai;
+
+chai.use(chaiHttp);
+
+let token = '';
+let testDirectoryId = '';
+const randomDirectoryName = `TestDir_${Date.now()}`; // Generate a unique name for each test run
+
+describe('Directory API', function () {
+  this.timeout(5000);
+
+  before((done) => {
+    const currentuser = {
+      email: 'test@gmail.com',
+      password: 'Airene@123',
+    };
+
+    chai
+      .request(app)
+      .post('/api/auth/login')
+      .send(currentuser)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        token = res.body.data;
+        done();
+      });
+  });
+
+  after(async function () {
+    if (testDirectoryId) {
+      await Directory.findByIdAndDelete(testDirectoryId);
+    }
+
+});
+
+  it('should create a new directory successfully', (done) => {
+    chai
+      .request(app)
+      .post('/api/directories')
+      .set('Authorization', `Bearer ${token}`)
+      .send({ name: randomDirectoryName })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equal('Successfully created directory');
+        testDirectoryId = res.body.data._id;
+        done();
+      });
+  
+  });
+
+  it('should not create a duplicate directory', (done) => {
+    chai
+    .request(app)
+    .post('/api/directories')
+    .set('Authorization', `Bearer ${token}`)
+    .send({ name: randomDirectoryName })
+    .end((err, res) => {
+      console.log('Response Body:', res.body); // Log response for debugging
+      console.log('Response Status:', res.status); // Log status code
+      expect(res).to.have.status(400); // Expect 400 Bad Request for duplicate directory
+      done();
+    });
+  
+  });
+
+  it('should retrieve all directories for the user', (done) => {
+    chai
+      .request(app)
+      .get('/api/directories')
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equal('Directories and files successfully retrieved');
+        expect(res.body.data).to.be.an('array');
+        done();
+      });
+  });
+
+  it('should retrieve a single directory by ID', (done) => {
+    chai
+      .request(app)
+      .get(`/api/directories/${testDirectoryId}`)
+      .set('Authorization', `Bearer ${token}`)
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        expect(res.body.message).to.equal('Directory and files successfully retrieved');
+        expect(res.body.data._id).to.equal(testDirectoryId);
+        done();
+      });
+  });
+});
